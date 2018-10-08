c8948e21c9d1be63ac90225c6a0e4be54b8346c1
Audrey <anfletcher24@gmail.com>
2018-10-08 16:36:16 -0500

    added singleWordMaker fucntion and working tests

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const sum = function (one, two){
-    if (typeof(one) === "string" || typeof(two) === "string") {
+    if (typeof(one) !== "number" || typeof(two) !== "number") {
         throw new Error('parameters must be a number')
     }
     return one + two;
 }
 
 const firstLetterOfWord = function(word) {
-    if (typeof(word) !== "string"){
-        throw new Error('mjust be string ')
+    if (typeof(word) !== "string" || word.includes(" ")){
+        throw new Error('must be a string of one word')
     }
-    
         return word[0];
-
 }
 
 // TODO: function that breaks sentence into multiple words
+const singleWordMaker =  function(input){
+    if (typeof(input) !== "string"){
+        throw new Error('must be a string')
+    }
+    else {
+        var words = input.split(" ");
+        for(i=0; i <words.length; i++){
+            console.log( words[i]);
+        }
+    }
+}
 
 module.exports = {
     sum: sum,
-    firstLetterOfWord: firstLetterOfWord
+    firstLetterOfWord: firstLetterOfWord,
+    singleWordMaker: singleWordMaker
 }; 
 
--- test/app.test.js
+++ test/app.test.js
@@ -2,12 +2,12 @@ var appFunctions = require("../app.js");
 var chai = require("chai");
 
 describe('sum', () => {
-    it ('returns the sum', () => {
+    it ('returns the sum of 3+4', () => {
         var example= appFunctions.sum(3,4);
         chai.assert.equal(example, 7)
     })
 
-    it ('returns the sum', () => {
+    it ('returns the sum of 0+4', () => {
         var example= appFunctions.sum(0,4);
         chai.assert.equal(example, 4)
     })
@@ -19,18 +19,18 @@ describe('sum', () => {
     })
 
     // TODO: get these to pass (update implementation)
-    // it ('throws an error when the parameter is not a number', () => {
-    //     chai.assert.throws(function() {
-    //         appFunctions.sum([], 0)
-    //     })
-    // })
+    it ('throws an error when the parameter is not a number', () => {
+        chai.assert.throws(function() {
+            appFunctions.sum([], 0)
+        })
+    })
 
     // TODO: get these to pass (update implementation)
-    // it ('throws an error when the parameter is not a number', () => {
-    //     chai.assert.throws(function() {
-    //         appFunctions.sum(undefined, 0)
-    //     })
-    // })
+    it ('throws an error when the parameter is not a number', () => {
+        chai.assert.throws(function() {
+            appFunctions.sum(undefined, 0)
+        })
+    })
 })
 
 describe('firstLetterOfWord', () => {
@@ -45,11 +45,46 @@ describe('firstLetterOfWord', () => {
         })
     })
 
-    // TODO:
-    // it('given input string: Prudence is my dog, throws error that input is multiple words', () => {
-        
-    // })
+    it('throws an error if the input is not a string', () => {
+        chai.assert.throws(function(){
+            appFunctions.firstLetterOfWord([]);
+        })
+    })
+
+    it('throws an error if the input is not a string', () => {
+        chai.assert.throws(function(){
+            appFunctions.firstLetterOfWord(undefined);
+        })
+    })
+
+    it('given input string: Prudence is my dog, throws error that input is multiple words', () => {
+        chai.assert.throws(function(){
+            appFunctions.firstLetterOfWord("Prudence is my dog");
+        })
+    })
 })
 
 // TODO: unit tests for function that breaks sentence into multiple words
 
+describe('singleWordMaker', () => {
+    it('throws an error if input is not a string', ()=>{
+        chai.assert.throws(function(){
+            appFunctions.singleWordMaker(4);
+        })
+    })
+    it('throws an error if input is not a string', ()=>{
+        chai.assert.throws(function(){
+            appFunctions.singleWordMaker([]);
+        })
+    })
+    it('throws an error if input is not a string', ()=>{
+        chai.assert.throws(function(){
+            appFunctions.singleWordMaker(undefined);
+        })
+    })
+    // it("thows an error if there is a space included in the output", ()=>{
+    //     chai.assert.throws(function(){
+    //         appFunctions.singleWordMaker();
+    //     }
+    // })
+})
